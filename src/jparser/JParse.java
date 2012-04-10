9fd76aacd60c78c4648f685584122768ff5290ff
zslevi <zslevi@53928f9b-d43b-4e92-82a7-ae2c468d2c32>
2012-04-10 15:33:22 +0000

    Fixed initial import.

---
+++ src/jparser/JParse.java
@@ -0,0 +1,32 @@
+package jparser;
+
+import jparser.numbered.NumberedString;
+
+
+
+public class JParse {
+	
+	private static ASTNode parse(String s) {
+		
+//		return new ASTNode(new NumberedString(s), null, NodeType.Complex,new Config());
+		return ASTNode.parse(new NumberedString(s),new Config());
+	}
+	
+	public static void main(String[] args) {
+//		ASTNode root = parse("('xy',\"b\",c,(asas,asd,\"w\\\"we\"))");
+//		ASTNode root = parse("[1,2] ('xy',\"b\",c,(asas,asd,\"w\\\"we\"))");
+//		ASTNode root = parse("begin c end; ('xy',\"b\",(asas,asd,\"w\\\"we\"))");
+	    
+		// faulty nesting
+	    ASTNode root = parse("('xy',\n\n" +
+	    		"\"b\",c,(asas,asd],\"w\\\"we\")(");
+	    
+//	    ASTNode root = parse("('xy',\n" +
+//        "\"b\",c,(asas,asd,\"w\\\"we\")))\nO");
+	    
+		System.out.print(root.toString());		
+	}
+
+	
+
+}
---
+++ src/jparser/NodeType.java
@@ -0,0 +1,5 @@
+package jparser;
+
+public enum NodeType {
+	Complex,Block,Atom,Quote,Apostrophe,Unparsed
+}
