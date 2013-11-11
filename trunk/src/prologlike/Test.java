d1b0904b1b2447ef048ec598cdb58a4b465fea90
zslevi <zslevi@53928f9b-d43b-4e92-82a7-ae2c468d2c32>
2013-11-11 17:41:16 +0000

    Prologlike stuff imported.

---
+++ trunk/src/prologlike/Test.java
@@ -0,0 +1,77 @@
+package prologlike;
+
+public class Test {
+    public static void main(String[] args) {
+        test1();
+    }
+
+    private static void test1() {
+        Integer[] ra = {1,2,3};
+        Integer[] rb = {1,2,3};
+        final Var<Integer> a = new Var<Integer>(ra);
+        final Var b = new Var<Integer>(rb);
+        Pred pred = new Pred() {
+            
+            @Override
+            protected boolean test() {
+                // TODO Auto-generated method stub
+                int ia = a.getCurrval();
+                int ib = (Integer) b.getCurrval();
+                if (ia+ib<3) 
+                    return true;
+                else 
+                    return false;
+            }
+        };
+        pred.addVar(a);
+        pred.addVar(b);
+        
+        boolean sol;
+
+        sol = pred.findNextSolution();        
+        System.out.println("a: "+a.getCurrval()+", b: "+b.getCurrval());
+        System.out.println("sol "+sol);
+        
+        sol = pred.findNextSolution();        
+        System.out.println("a: "+a.getCurrval()+", b: "+b.getCurrval());
+        System.out.println("sol "+sol);
+        
+        sol = pred.findNextSolution();        
+        System.out.println("a: "+a.getCurrval()+", b: "+b.getCurrval());
+        System.out.println("sol "+sol);
+        
+        sol = pred.findNextSolution();        
+        System.out.println("a: "+a.getCurrval()+", b: "+b.getCurrval());
+        System.out.println("sol "+sol);
+        
+        sol = pred.findNextSolution();        
+        System.out.println("a: "+a.getCurrval()+", b: "+b.getCurrval());
+        System.out.println("sol "+sol);
+        
+        sol = pred.findNextSolution();        
+        System.out.println("a: "+a.getCurrval()+", b: "+b.getCurrval());
+        System.out.println("sol "+sol);
+        
+        sol = pred.findNextSolution();        
+        System.out.println("a: "+a.getCurrval()+", b: "+b.getCurrval());
+        System.out.println("sol "+sol);
+        
+        sol = pred.findNextSolution();        
+        System.out.println("a: "+a.getCurrval()+", b: "+b.getCurrval());
+        System.out.println("sol "+sol);
+        
+        sol = pred.findNextSolution();        
+        System.out.println("a: "+a.getCurrval()+", b: "+b.getCurrval());
+        System.out.println("sol "+sol);
+
+        sol = pred.findNextSolution();        
+        System.out.println("a: "+a.getCurrval()+", b: "+b.getCurrval());
+        System.out.println("sol "+sol);
+        
+        sol = pred.findNextSolution();        
+        System.out.println("a: "+a.getCurrval()+", b: "+b.getCurrval());
+        System.out.println("sol "+sol);
+        
+        
+    }
+}
