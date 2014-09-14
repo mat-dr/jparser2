69435d959546f6de8f4defc5c2ca1bd235a9a740
zslevi <zslevi@53928f9b-d43b-4e92-82a7-ae2c468d2c32>
2014-09-14 13:05:11 +0000

    Fixed a bug in the patternmatching in Replace. Not sure yet what I'll do with the newly added util/iterators, but they were sitting on my HDD for a while and I didn't wanted to let them go waste.

---
+++ src/jparser/util/TreeIterator.java
@@ -0,0 +1,13 @@
+package jparser.util;
+
+import jparser.ASTNode;
+
+public abstract class TreeIterator {
+    abstract protected void processNode(ASTNode parent, ASTNode child, int childIndex);
+    protected void iterate(ASTNode node) {
+        for(int childIndex = 0; childIndex < node.children.size(); childIndex++) {
+            ASTNode child = node.children.get(childIndex);
+            processNode(node,child,childIndex);
+        }
+    }
+}
