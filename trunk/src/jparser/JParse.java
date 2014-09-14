1ba814fae1880df6d38193311af4d11bd9d54e06
zslevi <zslevi@53928f9b-d43b-4e92-82a7-ae2c468d2c32>
2014-09-14 16:51:38 +0000

    Comments added. Replace works fine, but parsePattern has a bug: ASTNode root = parsePattern("begin *wild* end;[[b+c]a]"); will generate a stack overflow.

--- trunk/src/jparser/JParse.java
+++ trunk/src/jparser/JParse.java
@@ -7,15 +7,16 @@ import jparser.util.XMLPrinter;
 
 public class JParse {
 	
+    /** Helper method for parsing. For custom block-delimiter definitions see Config. */ 
 	public static ASTNode parse(String s) {
 		
 //		return new ASTNode(new NumberedString(s), null, NodeType.Complex,new Config());
 		return ASTNode.parse(new NumberedString(s),new Config());
 	}
-	
-	   public static ASTNode parsePattern(String s) {
-	       return ASTNode.parsePattern(new NumberedString(s),new Config());
-	   }
+	/** Helper method to parse patterns. The wildcard identifier is defined in Config. */	
+   public static ASTNode parsePattern(String s) {
+       return ASTNode.parsePattern(new NumberedString(s),new Config());
+   }
 	
 	public static void main(String[] args) {
 //		ASTNode root = parse("('xy',\"b\",c,(asas,asd,\"w\\\"we\"))");
@@ -33,8 +34,9 @@ public class JParse {
 		
 //		ASTNode root = parse("begin x end;[[b+c]a]");
 	    
-	    ASTNode root = parse("begin *wild* end;[[b+c]a]");
-//	    ASTNode root = parsePattern("[[*wild*]*wild*]");
+//	    ASTNode root = parse("begin *wild* end;[[b+c]a]");
+	    ASTNode root = parsePattern("[[*wild*]*wild*]");
+//	    ASTNode root = parsePattern("begin *wild* end;[[b+c]a]");
 	    
 		System.out.println(root.toString());
 		
