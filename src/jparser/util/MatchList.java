2e319c9f6b3a0905b049346dcee04b7c3906aca4
zslevi <zslevi@53928f9b-d43b-4e92-82a7-ae2c468d2c32>
2015-12-05 22:23:37 +0000

    Fixed matchlist loop condition.

--- src/jparser/util/MatchList.java
+++ src/jparser/util/MatchList.java
@@ -1,5 +1,6 @@
 package jparser.util;
 
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
@@ -10,14 +11,14 @@ public class MatchList {
     public static List<Integer> matchLists(List pattern, List full){
         LinkedList<Integer> result = new LinkedList<Integer>();
         int indexFull = 0;
-        while(indexFull + pattern.size() < full.size()){
+        while(indexFull + pattern.size() <= full.size()){
             List slice = full.subList(indexFull, indexFull+pattern.size());
             if (matchesSliced(pattern, slice)){
                 result.add(indexFull);
             }
             indexFull++;
         }
-        return null;
+        return result;
     }
     
     /** It can be made later to unify too. */
@@ -26,7 +27,16 @@ public class MatchList {
     }
 
     public static void main(String[] args) {
-
+        
+        Integer[] full = new Integer[] {1,2,3,1,2};
+        Integer[] patt = new Integer[] {1,2};
+        
+        List<Integer> tmp = Arrays.asList(full);
+        
+        List<Integer> res = matchLists(Arrays.asList(patt), Arrays.asList(full));
+        System.out.println(res);
+        
+        
     }
 
 }
